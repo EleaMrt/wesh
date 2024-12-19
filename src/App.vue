19c78d0ec59970d2c09cb47f91d7c02afc6eaf74
EleaMrt <elxa24@gmail.com>
2024-12-19 10:57:29 +0100

    cleaning

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import  HeaderPage from '@/components/HeaderPage.vue'
+import  FooterPage from '@/components/FooterPage.vue'
+</script>
+
+
+<template>
+  <HeaderPage></HeaderPage>
+
+  <RouterView /> <!-- Si retire ca y a plus rien -->
+  
+  <FooterPage></FooterPage>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
